2b263359c28fa73a8fd9e976966c6326bc25e6fb
Te Vallee <astronomemusic@gmail.com>
2014-12-16 20:01:03 -1000

    not working

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,79 +1,126 @@
 $(function() {
+
   // on page load, get all todos
+  
   $.get('/items', function (docs) {
-    // render
-    docs.forEach(function (docs) {
-      $('ul#todo_list').append(buildTodoItem( doc ));
-    });
+    
+    // render items
+    
+    docs.forEach(function (doc) {
+      
+      $('ul#todo_list').append( buildTodoItem( doc ) )
+
+    })
+
   });
-  // on ENTER keydown, save new todo item 
-  $('target').keydown(function(event) {
-    if(event.which == 13) {
+
+
+  // on press enter, save new todo
+
+  $( "#target" ).keydown(function(event) {
+    if( event.which == 13 ) {
       createNewTodo();
     }
   });
 
-  // HELPER METHODS
-  function buildTodoItem(todo_doc) {
-    var list_item = $('<li>', {
-      class: 'list_items',
-      'data-object-id': todo_doc._id
+
+
+
+
+
+  /*
+  
+    HELPER METHODS
+   */
+
+  // return a <li> jQuery elem
+  /*
+    todo_doc = {
+      _id : "32 bit hex",
+      title : "string",
+      completed : "boolean as string"
+    }
+   */
+  function buildTodoItem ( todo_doc ) {
+    
+    var list_item = $('<li>',{
+      class : "list_items",
+      "data-object-id" : todo_doc._id
     });
-  }
 
-  var list_label = $('<span>', {
-    text: 'delete',
-    click: click_delete_item_handler
-  });
+    var list_checkbox = $('<input>', {
+      type : "checkbox",
+      change : change_completed_status
+    });
 
-  if(todo_doc.completed === 'true') {
-    list_checkbox.attr('checked', 'checked');
-  }
+    var list_label = $('<span>', {
+      text : todo_doc.title
+    });
 
-  list_item
-    .append(list_checkbox)
-    .append(list_label)
-    .append(list_delete);
+    var list_delete = $('<button>', {
+      text : "[delete]",
+      click : click_delete_item_handler
+    });
 
+    if(todo_doc.completed === "true") {
+      list_checkbox.attr("checked","checked");
+    }
+
+    list_item
+      .append( list_checkbox )
+      .append( list_label )
+      .append( list_delete );
+    
     return list_item;
   }
 
-  function createNewTodo() {
-    car user_input = $('#target').val();
-
-    // resets placeholder
+  //
+  function createNewTodo () {
+    
+    var user_input = $('#target').val();
+    
+    // RESETS TO PLACEHOLDER
     $('input').val('');
 
     var post_data = {
-      new_item: {
-        title: user_input,
-        checked: false
+      new_item : {
+        title : user_input,
+        checked : false
       }
     }
-  
 
-    $.post('/items', post_data, function(new_todo_id) {
+    $.post('/items', post_data, function (new_todo_id){
+      
       post_data.new_item._id = new_todo_id;
-      $('ul#todo_list').append(buildTodoItem(post_data.new_item));
-    });
-  }
-  // event handlers 
-  // http delete then delete it's parent li
-  function click_delete_item_handler (e) {
-    var button = $(e.currentTarget);
-    var parent_li = button.closest('li');
-    var object_id = parent_li.data('object-id');
+      $('ul#todo_list').append( buildTodoItem( post_data.new_item ) );
 
-    $.ajax('/items' + object_id + '/' + checkbox.prop('checked'),
-        {
-          type: 'PUT',
-          success: function(data) {
-            console.log('data', data);
-          }
-        }
-      );
-    }
+    });
+
   }
+
+  /*
+    
+    EVENT HANDLERS
+  */
+
+  // http DELETE then delete it's parent li
+  function click_delete_item_handler (e) {
+
+    var button = $(e.currentTarget);
+    var parent_li = button.closest("li");
+    var object_id = parent_li.data("object-id");
+
+    $.ajax( '/items/' + object_id ,
+      {
+        type : "DELETE",
+        success : function (data) {
+          parent_li.remove();
+          console.log('data',data);
+        }
+      }
+    );
+  }
+
   // http PUT to update completed status
   function change_completed_status (e) {
     var checkbox = $( e.currentTarget );
@@ -90,6 +137,11 @@ $(function() {
     );
 
   }
+
+
+
+  
+
   // CROSS OUT LIST ITEM IF BOX CHECKED
   $('ul').on('change', 'input[type=checkbox]', function() {
     if(this.checked) {
